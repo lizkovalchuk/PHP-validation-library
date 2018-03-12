44a827a903d8b478d8f1a8d936ac1dbf44887c9e
Liz Kovalchuk <liz.kovalchuk@gmail.com>
2018-03-12 11:32:04 -0400

    library in class and password & unsername validation

--- validation-library.php
+++ validation-library.php
@@ -1,35 +1,57 @@
 <?php
-function validateEmail($emailInput) : bool {
-    $emailPattern = "/([A-Za-z0-9_\-.]){1,}@([A-Za-z0-9_\-.]){1,}\.([A-Za-z]){2,4}/";
 
-    return (preg_match($emailPattern, $emailInput));
-}
+class Validation {
 
-function checkMinCharacters(string $textInput, int $maxCharCount) : bool {
+    function validateEmail($emailInput) : bool {
+        $emailPattern = "/([A-Za-z0-9_\-.]){1,}@([A-Za-z0-9_\-.]){1,}\.([A-Za-z]){2,4}/";
 
-    return (strlen($textInput) <= $maxCharCount);
-}
+        return (preg_match($emailPattern, $emailInput));
+    }
 
-function validateName($name) : bool {
-    $namePattern = '/^[a-zA-Z-\'\s]+$/';
+    function checkMinCharacters(string $textInput, int $maxCharCount) : bool {
 
-    return (preg_match($namePattern, $name));
-}
+        return (strlen($textInput) <= $maxCharCount);
+    }
 
-function validateDropdown($dropdownName) : bool {
-    return filter_input(INPUT_POST, $dropdownName);
-}
+    function validateName($name) : bool {
+        $namePattern = '/^[a-zA-Z-\'\s]+$/';
 
-function validateCheckbox($checkboxName) : bool {
-    return !empty($_POST[$checkboxName]);
-}
+        return (preg_match($namePattern, $name));
+    }
 
-function validatePhone($phone) : bool {
-    $phonePattern = "/(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/";
+    function validateDropdown($dropdownName) : bool {
+        return filter_input(INPUT_POST, $dropdownName);
+    }
 
-    return (preg_match($phonePattern, $phone));
-}
+    function validateCheckbox($checkboxName) : bool {
+        return !empty($_POST[$checkboxName]);
+    }
 
-function validateRadioButton($radioName) : bool {
-    return isset($_POST[$radioName]);
+    function validatePhone($phone) : bool {
+        $phonePattern = "/(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/";
+
+        return (preg_match($phonePattern, $phone));
+    }
+
+    function validateRadioButton($radioName) : bool {
+        return isset($_POST[$radioName]);
+    }
+
+    function validatePassword($passwordInput, $userPassword) : bool {
+        if ($passwordInput == $userPassword){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    function validateUsername($usernameInput, $userName) : bool {
+        if ($usernameInput == $userName){
+            return true;
+        } else{
+            return false;
+        }
+
+    }
+
 }

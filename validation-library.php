b8d6685ae4cf33b6e4355310ff588a233d0ad9d6
hazeoscar <yeganeh.ghs@gmail.com>
2018-03-12 19:07:56 -0400

    Yeganeh added public static to functions

--- validation-library.php
+++ validation-library.php
@@ -2,42 +2,42 @@
 
 class Validation {
 
-    function validateEmail($emailInput) : bool {
+    public static function validateEmail($emailInput) : bool {
         $emailPattern = "/([A-Za-z0-9_\-.]){1,}@([A-Za-z0-9_\-.]){1,}\.([A-Za-z]){2,4}/";
 
         return (preg_match($emailPattern, $emailInput));
     }
 
-    function checkMinCharacters(string $textInput, int $maxCharCount) : bool {
+    public static function checkMinCharacters(string $textInput, int $maxCharCount) : bool {
 
         return (strlen($textInput) <= $maxCharCount);
     }
 
-    function validateName($name) : bool {
+    public static function validateName($name) : bool {
         $namePattern = '/^[a-zA-Z-\'\s]+$/';
 
         return (preg_match($namePattern, $name));
     }
 
-    function validateDropdown($dropdownName) : bool {
+    public static function validateDropdown($dropdownName) : bool {
         return filter_input(INPUT_POST, $dropdownName);
     }
 
-    function validateCheckbox($checkboxName) : bool {
+    public static function validateCheckbox($checkboxName) : bool {
         return !empty($_POST[$checkboxName]);
     }
 
-    function validatePhone($phone) : bool {
+    public static function validatePhone($phone) : bool {
         $phonePattern = "/(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}/";
 
         return (preg_match($phonePattern, $phone));
     }
 
-    function validateRadioButton($radioName) : bool {
+    public static function validateRadioButton($radioName) : bool {
         return isset($_POST[$radioName]);
     }
 
-    function validatePassword($passwordInput, $userPassword) : bool {
+    public static function validatePassword($passwordInput, $userPassword) : bool {
         if ($passwordInput == $userPassword){
             return true;
         } else{
@@ -45,7 +45,7 @@ class Validation {
         }
     }
 
-    function validateUsername($usernameInput, $userName) : bool {
+    public static function validateUsername($usernameInput, $userName) : bool {
         if ($usernameInput == $userName){
             return true;
         } else{
